a392dcf1469fef1ad04155bcf41a26e8b07e4cb1
Simon Ozkan <110454261+SimonOzkan@users.noreply.github.com>
2025-09-16 15:36:52 +0200

    Add dependencies.R for package management

---
+++ Ferrybox scripts/dependencies.R
@@ -0,0 +1,17 @@
+# dependencies.R
+# Ensure pak is installed
+if (!requireNamespace("pak", quietly = TRUE)) {
+  install.packages("pak", repos = sprintf(
+    "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
+    .Platform$pkgType, R.Version()$os, R.Version()$arch
+  ))
+}
+
+# Install required CRAN packages
+pak::pkg_install(c(
+  "dplyr",
+  "tidyverse",
+  "tidyr",
+  "ncdf4",
+  "ggplot2"
+))
