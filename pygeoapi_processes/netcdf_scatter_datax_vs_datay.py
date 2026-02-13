c0f1398bd94b6acc167a51f384453551674a2341
Merret Buurman <merret.buurman@igb-berlin.de>
2026-02-13 13:54:33 +0000

    pygeoapi: Added first version of four new processes. Untested. Shapefile input almost certain to fail, due to not handling zips.

---
+++ pygeoapi_processes/netcdf_scatter_datax_vs_datay.py
@@ -0,0 +1,200 @@
+import logging
+from pygeoapi.process.base import BaseProcessor, ProcessorExecuteError
+LOGGER = logging.getLogger(__name__)
+
+import json
+import os
+import traceback
+import datetime
+import requests
+# niva repo has hyphen in it, so we cannot import it in the normal python way:
+#from pygeoapi.process.niva-aquainfra.pygeoapi_processes.docker_utils import run_docker_container3
+import importlib  
+docker_utils = importlib.import_module("pygeoapi.process.niva-aquainfra.pygeoapi_processes.docker_utils")
+
+
+'''
+# NOT TESTED YET
+curl -X POST https://${PYSERVER}/processes/netcdf-scatter-datax-vs-datay/execution \
+--header 'Content-Type: application/json' \
+--data '{
+    "inputs": {
+        "url_input_csv": "https://something.csv",
+        "lat_min": 59.1,
+        "lat_max": 59.2
+    }
+}'; date
+
+# NOT TESTED YET
+# NEED EXAMPLE PARAMETERS FROM NIVA (these were copied from some other process)
+curl -X POST https://${PYSERVER}/processes/netcdf-scatter-datax-vs-datay/execution \
+--header 'Content-Type: application/json' \
+--data '{
+    "inputs": {
+        "url_input_csv": "https://something.csv",
+        "url_input_waterbody": "https://something.zip",
+        "waterbody_ids_to_summarize": ["id1", "id2", "id3"],
+        "waterbody_id_to_be_used": "id3",
+        "latitude_min": 59.1,
+        "latitude_max": 59.2
+    }
+}'; date
+'''
+
+
+# Process metadata and description
+# Has to be in a JSON file of the same name, in the same dir! 
+script_title_and_path = __file__
+metadata_title_and_path = script_title_and_path.replace('.py', '.json')
+PROCESS_METADATA = json.load(open(metadata_title_and_path))
+
+
+
+class NivaNetcdfScatterDataxVsDatayProcessor(BaseProcessor):
+
+    def __init__(self, processor_def):
+        super().__init__(processor_def, PROCESS_METADATA)
+        self.job_id = None
+        self.process_id = self.metadata["id"]
+        config_file_path = os.environ.get('AQUAINFRA_CONFIG_FILE', "./config.json")
+        with open(config_file_path, 'r') as config_file:
+            config = json.load(config_file)
+            self.download_dir = config["download_dir"].rstrip('/')
+            self.download_url = config["download_url"].rstrip('/')
+            self.docker_executable = config["docker_executable"]
+            self.image_name = "ferry-rscripts:20260213"
+            self.script_name = 'netcdf_scatter_datax_vs_datay.R'
+
+
+    def set_job_id(self, job_id: str):
+        self.job_id = job_id
+
+    def __repr__(self):
+        return f'<NivaNetcdfScatterDataxVsDatayProcessor> {self.name}'
+
+
+    def execute(self, data):
+        LOGGER.info('Starting process NIVA Ferrybox: netcdf_scatter_datax_vs_datay.R!')
+        try:
+            mimetype, result = self._execute(data)
+            return mimetype, result
+
+        except Exception as e:
+            err_msg = str(e)
+            LOGGER.error(f'Error during execute: {err_msg}')
+            # This seems to look the same:
+            #LOGGER.error(f'Error during execute: {e}')
+            LOGGER.error('TRACEBACK:')
+            print(traceback.format_exc())
+            raise ProcessorExecuteError(err_msg) from e
+
+
+    def _execute(self, data):
+
+        ##############
+        ### Inputs ###
+        ##############
+
+        # Retrieve user inputs:
+        url_input_csv = data.get('url_input_csv')
+        url_input_waterbody = data.get('url_input_waterbody', None) # optional
+        waterbody_ids_to_summarize = data.get('waterbody_ids_to_summarize', None) # optional
+        waterbody_id_to_be_used = data.get('waterbody_id_to_be_used', None) # optional
+        latitude_min = data.get('latitude_min', None) # optional
+        latitude_max = data.get('latitude_max', None) # optional
+
+        # Check user inputs:
+        if url_input_csv is None:
+            raise ProcessorExecuteError("Missing parameter 'url_input_csv'. Please provide a URL.")
+
+        # Check validity of argument:
+        if latitude_min is not None:
+            float(latitude_min)
+        if latitude_max is not None:
+            float(latitude_max)
+
+        # Check existence:
+        requests.head(url_input_csv), raise_for_status()
+        if url_input_waterbody is not None:
+            requests.head(url_input_waterbody), raise_for_status()
+
+        ##################
+        ### Input data ###
+        ##################
+
+        # Where to store input data (will be mounted read-write into container):
+        #input_dir = f'{self.download_dir}/in/{self.process_id}/job_{self.job_id}'
+        #os.makedirs(input_dir, exist_ok=True)
+        # Not needed, no input data is downloaded!
+        input_dir = None
+
+        # Directory where static input data can be found (will be mounted readonly into container):
+        # Not needed, no input data is downloaded!
+        readonly_dir = None
+
+
+        ###############
+        ### Outputs ###
+        ###############
+
+        # Where to store output data
+        output_dir = f'{self.download_dir}/out/{self.process_id}/job_{self.job_id}'
+        output_url = f'{self.download_url}/out/{self.process_id}/job_{self.job_id}'
+        os.makedirs(output_dir, exist_ok=True)
+        LOGGER.debug(f'All results will be stored     in: {output_dir}')
+        LOGGER.debug(f'All results will be accessible in: {output_url}')
+        # Output filename
+        out_result_path = f'{output_dir}/assessment_area_{self.job_id}.png'
+        out_result_url  = f'{output_url}/assessment_area_{self.job_id}.png'
+
+        ###########
+        ### Run ###
+        ###########
+
+        latitude_range = f'c({latitude_min}, {latitude_max})'
+        r_args = [
+            url_input_csv,
+            out_result_path,
+            url_input_waterbody,
+            waterbody_ids_to_summarize,
+            waterbody_id_to_be_used,
+            latitude_range
+        ]
+
+        r_args = [url_input_csv, out_result_path, url_input_river_logger_csv, url_input_waterbody]
+        LOGGER.debug(f"r_args: {r_args}")
+        returncode, stdout, stderr, user_err_msg = docker_utils.run_docker_container3(
+            self.docker_executable,
+            self.image_name,
+            self.script_name,
+            output_dir,
+            r_args
+        )
+
+        # Return R error message if exit code not 0:
+        if not returncode == 0:
+            raise ProcessorExecuteError(user_msg = user_err_msg)
+
+
+
+        ######################
+        ### Return results ###
+        ######################
+
+        # Return link to output file wrapped in JSON:
+        outputs = {
+            "outputs": {
+                "scatterplot": {
+                    "title": PROCESS_METADATA['outputs']['scatterplot']['title'],
+                    "description": PROCESS_METADATA['outputs']['scatterplot']['description'],
+                    "href": out_result_url
+                }
+            }
+        }
+
+        return 'application/json', outputs
+
+
+
+
+
