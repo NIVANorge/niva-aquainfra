e752cbd3955b40c5dbcf217b19ad36aeb0093686
Simon Ozkan <110454261+SimonOzkan@users.noreply.github.com>
2026-03-13 14:36:28 +0100

    Remove unused parameter joining in netcdf_assessment_area.py
    Commented out unused parameter joining code.

--- pygeoapi_processes/netcdf_assessment_area.py
+++ pygeoapi_processes/netcdf_assessment_area.py
@@ -141,7 +141,7 @@ class NivaNetcdfAssessmentAreaProcessor(BaseProcessor):
         ### Run ###
         ###########
 
-        params = ','.join(parameters)
+       # params = ','.join(parameters) parameter not used in script
         r_args = [url_input_csv, out_result_path, url_input_river_logger_csv, url_input_waterbody]
         LOGGER.debug(f"r_args: {r_args}")
         returncode, stdout, stderr, user_err_msg = docker_utils.run_docker_container3(
