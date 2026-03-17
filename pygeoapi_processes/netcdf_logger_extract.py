900649352e7e1dbcbfd7f5322b21a7cccba5f42b
Merret Buurman <merret.buurman@igb-berlin.de>
2026-03-17 14:13:36 +0000

    Pygoeapi: Fixed malformed curl request.

--- pygeoapi_processes/netcdf_logger_extract.py
+++ pygeoapi_processes/netcdf_logger_extract.py
@@ -14,17 +14,16 @@ docker_utils = importlib.import_module("pygeoapi.process.niva-aquainfra.pygeoapi
 
 
 '''
-# NOT TESTED YET - Have tested and works - SIMON: 17/03-2026
+# TESTED, works only with NULL parameters, 2026-03-17
 curl -X POST https://${PYSERVER}/processes/netcdf-logger-extract/execution \
 --header 'Content-Type: application/json' \
 --data '{
-  "source": "https://thredds.niva.no/thredds/dodsC/datasets/loggers/glomma/baterod.nc",
-  "save_path": "/out/logger_test.csv",
-  "parameters": "NULL",  # Can be NULL or all or one of temp_water_avg, phvalue_avg, condvalue_avg, turbidity_avg, cdomdigitalfinal
-  "start_date": "2023-01-01",
-  "end_date": "2023-12-31",
-  "image": "ferry-rscripts:latest"
-}
+    "inputs": {
+       "url_thredds": "https://thredds.niva.no/thredds/dodsC/datasets/loggers/glomma/baterod.nc",
+       "parameters": "NULL",
+       "start_date": "2023-01-01",
+       "end_date": "2023-12-31"
+    }
 }'; date
 
 # NOT TESTED YET
