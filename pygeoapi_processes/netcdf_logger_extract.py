81ded07a3e0b8430f5e60494a5cbb182be98e5bb
Simon Ozkan <110454261+SimonOzkan@users.noreply.github.com>
2026-07-08 10:43:22 +0200

    Update netcdf_logger_extract.py

--- pygeoapi_processes/netcdf_logger_extract.py
+++ pygeoapi_processes/netcdf_logger_extract.py
@@ -7,8 +7,6 @@ import os
 import traceback
 import datetime
 import requests
-# niva repo has hyphen in it, so we cannot import it in the normal python way:
-#from pygeoapi.process.niva-aquainfra.pygeoapi_processes.docker_utils import run_docker_container3
 import importlib  
 docker_utils = importlib.import_module("pygeoapi.process.niva-aquainfra.pygeoapi_processes.docker_utils")
 
@@ -39,16 +37,13 @@ curl -X POST https://${PYSERVER}/processes/netcdf-logger-extract/execution \
         "end_date": "2023-12-31"
     }
 }'; date
-
 '''
 
 
 # Process metadata and description
-# Has to be in a JSON file of the same name, in the same dir! 
 script_title_and_path = __file__
 metadata_title_and_path = script_title_and_path.replace('.py', '.json')
 PROCESS_METADATA = json.load(open(metadata_title_and_path))
-
 
 
 class NivaNetcdfLoggerExtractProcessor(BaseProcessor):
@@ -57,6 +52,7 @@ class NivaNetcdfLoggerExtractProcessor(BaseProcessor):
         super().__init__(processor_def, PROCESS_METADATA)
         self.job_id = None
         self.process_id = self.metadata["id"]
+
         config_file_path = os.environ.get('AQUAINFRA_CONFIG_FILE', "./config.json")
         with open(config_file_path, 'r') as config_file:
             config = json.load(config_file)
@@ -66,29 +62,23 @@ class NivaNetcdfLoggerExtractProcessor(BaseProcessor):
             self.image_name = "ferry-rscripts:20260422-b0a726b"
             self.script_name = 'netcdf_logger_extract.R'
 
-
     def set_job_id(self, job_id: str):
         self.job_id = job_id
 
     def __repr__(self):
         return f'<NivaNetcdfLoggerExtractProcessor> {self.name}'
 
-
     def execute(self, data):
         LOGGER.info(f'Starting process NIVA Ferrybox: {self.script_name}')
         try:
             mimetype, result = self._execute(data)
             return mimetype, result
-
         except Exception as e:
             err_msg = str(e)
             LOGGER.error(f'Error during execute: {err_msg}')
-            # This seems to look the same:
-            #LOGGER.error(f'Error during execute: {e}')
             LOGGER.error('TRACEBACK:')
             print(traceback.format_exc())
             raise ProcessorExecuteError(err_msg) from e
-
 
     def _execute(self, data):
 
@@ -96,7 +86,6 @@ class NivaNetcdfLoggerExtractProcessor(BaseProcessor):
         ### Inputs ###
         ##############
 
-        # Retrieve user inputs:
         url_thredds = data.get('url_thredds')
         start_date = data.get('start_date')
         end_date = data.get('end_date')
@@ -110,73 +99,60 @@ class NivaNetcdfLoggerExtractProcessor(BaseProcessor):
         if end_date is None:
             raise ProcessorExecuteError("Missing parameter 'end_date'. Please provide a date (yyyy-mm-dd).")
 
-        # Check validity of argument:
-        # Parse and validate the dates, to see whether it is valid:
+        # Validate dates:
         parsed_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
         LOGGER.debug(f'The provided start_date is valid: {parsed_date}')
         parsed_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
         LOGGER.debug(f'The provided end_date is valid:   {parsed_date}')
 
-        # Check existence:
-        # Note: During testing, this gets HTTP 400. Maybe THREDDS does not reply to HEAD requests.
-        #requests.head(url_thredds).raise_for_status()
-
         ##################
         ### Input data ###
         ##################
 
-        # Where to store input data (will be mounted read-write into container):
-        #input_dir = f'{self.download_dir}/in/{self.process_id}/job_{self.job_id}'
-        #os.makedirs(input_dir, exist_ok=True)
-        # Not needed, no input data is downloaded!
         input_dir = None
-
-        # Directory where static input data can be found (will be mounted readonly into container):
-        # Not needed, no input data is downloaded!
         readonly_dir = None
-
-
-        ###############
-        ### Outputs ###
-        ###############
-
-        # Where to store output data
-        output_dir = f'{self.download_dir}/out/{self.process_id}/job_{self.job_id}'
-        output_url = f'{self.download_url}/out/{self.process_id}/job_{self.job_id}'
-        os.makedirs(output_dir, exist_ok=True)
-        LOGGER.debug(f'All results will be stored     in: {output_dir}')
-        LOGGER.debug(f'All results will be accessible in: {output_url}')
-        # Output filename
-        out_result_path = f'{output_dir}/logger_{self.job_id}.csv'
-        out_result_url  = f'{output_url}/logger_{self.job_id}.csv'
 
         ###########
         ### Run ###
         ###########
 
-        # Make a proper string from the parameters that we can pass over to the R script:
-        #if parameters is not None:
-        #    parameters = ','.join(parameters)
-# Make a proper string from the parameters that we can pass over to the R script:
+        # Convert parameters to string first - used in filename and R args
         if parameters is not None:
             if isinstance(parameters, list):
-                parameters = ','.join(parameters)   # Handles list
+                params_string   = ','.join(parameters)   # til R args: "turbidity_avg,temperature"
+                params_filename = '-'.join(parameters)   # til filnavn: "turbidity_avg-temperature"
             elif isinstance(parameters, str):
-                parameters = parameters             # Handles single input
+                params_string   = parameters
+                params_filename = parameters.replace(',', '-')
+        else:
+            params_string   = None
+            params_filename = "all"
 
+        ###############
+        ### Outputs ###
+        ###############
 
-        
+        output_dir = f'{self.download_dir}/out/{self.process_id}/job_{self.job_id}'
+        output_url = f'{self.download_url}/out/{self.process_id}/job_{self.job_id}'
+        os.makedirs(output_dir, exist_ok=True)
+        LOGGER.debug(f'All results will be stored     in: {output_dir}')
+        LOGGER.debug(f'All results will be accessible in: {output_url}')
+
+        # Output filnavn inkluderer nu parameter-navn
+        out_result_path = f'{output_dir}/logger_{self.job_id}_{params_filename}.csv'
+        out_result_url  = f'{output_url}/logger_{self.job_id}_{params_filename}.csv'
+
         # Assemble R args:
         r_args = [
             url_thredds,
             out_result_path,
-            parameters,
+            params_string,
             start_date,
             end_date
         ]
         LOGGER.debug(f"r_args: {r_args}")
 
-        # Actually call R script:
+        # Call R script:
         returncode, stdout, stderr, user_err_msg = docker_utils.run_docker_container3(
             self.docker_executable,
             self.image_name,
@@ -185,17 +161,13 @@ class NivaNetcdfLoggerExtractProcessor(BaseProcessor):
             r_args
         )
 
-        # Return R error message if exit code not 0:
         if not returncode == 0:
-            raise ProcessorExecuteError(user_msg = user_err_msg)
-
-
+            raise ProcessorExecuteError(user_msg=user_err_msg)
 
         ######################
         ### Return results ###
         ######################
 
-        # Return link to output file wrapped in JSON:
         outputs = {
             "outputs": {
                 "output_csv": {
@@ -207,8 +179,6 @@ class NivaNetcdfLoggerExtractProcessor(BaseProcessor):
         }
 
         return 'application/json', outputs
-
-
 
 
 
